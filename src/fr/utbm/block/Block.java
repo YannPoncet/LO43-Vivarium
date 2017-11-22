6cd49ab7562e859e36cf7969d11304648f3b826b
rodol <rodol@Rodolphe1997>
2017-11-22 18:37:36 +0100

    Block update 1

--- src/fr/utbm/block/Block.java
+++ src/fr/utbm/block/Block.java
@@ -1,14 +1,44 @@
 package fr.utbm.block;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import fr.utbm.render.Renderable;
+import fr.utbm.texture.TextureManager;
 
 public class Block extends Renderable{
 	
+	protected int blockId;
+	protected int maxHealth;
+	protected int blockHealth;
+	protected BlockType blockType;
+	protected boolean isGravitySensitive;
+	protected Texture breakingStage;
+	
 	public Block(float x, float y,Texture text) {
 		super(x*16, y*16, text);
+		breakingStage = null;
 	}
 	
+	public void update()
+	{
+		
+	}
+	
+	public void damage(int dmg)
+	{
+		this.blockHealth =- dmg;
+		int i = (blockHealth / maxHealth) * 100 / 25;
+		breakingStage = TextureManager.getTexture(200 + i);
+	}
+	
+	@Override
+	public void render(SpriteBatch batch){
+		batch.draw(text, x, y);
+		if(breakingStage != null)
+		{
+			batch.draw(breakingStage, x, y);
+		}
+	}
 
 }
--- src/fr/utbm/block/BlockDirt.java
+++ src/fr/utbm/block/BlockDirt.java
@@ -1,27 +1,26 @@
 package fr.utbm.block;
 
-import com.badlogic.gdx.graphics.Texture;
-
 import fr.utbm.texture.TextureManager;
 
-public class BlockGrass extends Block{
-
-	public BlockGrass(float x, float y) {
-		super(x, y, TextureManager.getTexture(0));
-		this.blockId = 0;
-		this.maxHealth = 100;
+public class BlockDirt extends Block{
+	
+	public BlockDirt(float x, float y)
+	{
+		super(x, y, TextureManager.getTexture(1));
+		this.blockId = 1;
 		this.blockHealth = 100;
+		this.maxHealth = 100;
 		this.blockType = BlockType.DIRT;
 		this.isGravitySensitive = false;
 	}
 	
-	public BlockGrass(float x, float y, int bH) {
+	public BlockDirt(float x, float y, int bH) {
 		super(x, y, TextureManager.getTexture(0));
 		this.blockId = 0;
 		this.maxHealth = 100;
-		this.blockHealth = 100;
+		this.blockHealth = bH;
 		this.blockType = BlockType.DIRT;
 		this.isGravitySensitive = false;
-		super.damage(maxHealth-blockHealth);
 	}
+
 }
