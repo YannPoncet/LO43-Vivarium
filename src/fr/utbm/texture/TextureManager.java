4a0d93fa5b042602f33fce3a4ba36a58982d5bbc
UTBMRidd <cedric.paquet@utbm.fr>
2017-11-22 11:16:23 +0100

    Explications TextureManager

--- src/fr/utbm/texture/TextureManager.java
+++ src/fr/utbm/texture/TextureManager.java
@@ -9,10 +9,18 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.XmlReader;
 import com.badlogic.gdx.utils.XmlReader.Element;
 
+/* L'Objectif de cette classe c'est de charger toutes les textures avant qu'on joue et de les classer pour deux raisons : 1- Si on charge les textures au fur et à mesure quand on joue, ca risque de faire lagger,
+ * C'est l'interet de faire un écran de chargement	2- Si je fais deux blocks de terre, il faudra charger 2 fois la texture du blockTerre, avec le textureManager, elle sera chargé une seule fois peu importe le nb de blocks
+ */
+
 public class TextureManager {
 	
 	private static HashMap<Integer,Texture> textureList= new HashMap<Integer, Texture>();
 	
+	
+	/* Permets d'obtenir une texture pré-chargé grâce à son ID
+	 * IL FAUT AVOIR FAIT UN LOAD AVANT
+	 */
 	public static Texture getTexture(int id){
 		Texture t = textureList.get(id);
 		if(t == null){
@@ -21,9 +29,13 @@ public class TextureManager {
 		}
 		return t;
 	}
+	
+	/* Ajoute une texture à la HashMap */
 	private static void addText(int id, Texture text){
 		textureList.put(id, text);
 	}
+	
+	/* Charge toutes les textures présentes dans "res/textures/textureManager.xml" */
 	public static void loadTextures(){
 		
 		String xmlFile = "res/textures/textureManager.xml";
