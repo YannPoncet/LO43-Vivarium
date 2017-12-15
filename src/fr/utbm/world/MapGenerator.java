1211b2faf096a6b778f78997132938fa024dbd25
BorisBrogle <boris.brogle@gmail.com>
2017-12-15 16:10:42 +0100

    The entire map is now genrated [minor change]

--- src/fr/utbm/world/MapGenerator.java
+++ src/fr/utbm/world/MapGenerator.java
@@ -7,14 +7,14 @@ public class MapGenerator {
 
 	public static void generate(World world)
 	{
-			for(int i=0; i<Chunk.CHUNK_WIDTH;i++)
+			for(int i=0; i<Map.NUMBER_OF_CHUNKS*Chunk.CHUNK_WIDTH;i++)
 			{
 				for(int j=0; j<Chunk.CHUNK_HEIGHT/2;j++)
 				{
 					world.setBlock(i, j, new BlockDirt(i,j));
 				}
 			}
-			for(int i=0; i<Chunk.CHUNK_WIDTH;i++)
+			for(int i=0; i<Map.NUMBER_OF_CHUNKS*Chunk.CHUNK_WIDTH;i++)
 			{
 				world.setBlock(i, Chunk.CHUNK_HEIGHT/2, new BlockGrass(i,Chunk.CHUNK_HEIGHT/2));
 			}
