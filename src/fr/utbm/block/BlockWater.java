3d79a990a8d22f3ea20360fe7928269b52ca84c3
rodol <rodol@Rodolphe1997>
2017-12-22 14:10:32 +0100

    Without syso

--- src/fr/utbm/block/BlockWater.java
+++ src/fr/utbm/block/BlockWater.java
@@ -23,7 +23,7 @@ public class BlockWater extends BlockLiquid{
 	{
 		if(state > 7)
 		{
-			System.out.println("J'ai kill le bloc en " + this.x/16 +" ; "+ this.y/16);
+			//System.out.println("J'ai kill le bloc en " + this.x/16 +" ; "+ this.y/16);
 			dead = true;
 		}
 		else
@@ -41,35 +41,35 @@ public class BlockWater extends BlockLiquid{
 		if(iter == 10)
 		{
 			//void block around
-			System.out.println("je suis rentré dans le test de l'update du bloc en " + (int)(this.x/16) +" ; "+ (int)(this.y/16));
+			//System.out.println("je suis rentré dans le test de l'update du bloc en " + (int)(this.x/16) +" ; "+ (int)(this.y/16));
 			if(world.getBlock((int)(this.x/16), (int)((this.y/16)-1)) == null)
 			{
-				System.out.println("je descends pour la premiere fois");
+				//System.out.println("je descends pour la premiere fois");
 				BlockWater block = new BlockWater(x/16, (y/16)-1, 7, world);
 				block.flowing = Direction.DOWN;
 				world.setBlock(((int)(this.x/16)), ((int)(this.y/16))-1, block);
 				this.state ++;
-				System.out.println("Son état est : " + ((BlockLiquid)world.getBlock(((int)(this.x/16)), ((int)((this.y/16)-1)))).state);
+				//System.out.println("Son état est : " + ((BlockLiquid)world.getBlock(((int)(this.x/16)), ((int)((this.y/16)-1)))).state);
 			}
 			
 			
 			//same liquid block around
 			else if(world.getBlock((int)(this.x/16), (int)((this.y/16)-1)).blockId == this.blockId)
 			{
-				System.out.println("je prolonge la descente");
+				//System.out.println("je prolonge la descente");
 				if(((BlockLiquid)world.getBlock((int)(this.x/16), (int)((this.y/16)-1))).state > 0)
 				{
-					System.out.println("J'actualise l'autre bloc");
+					//System.out.println("J'actualise l'autre bloc");
 					//flowing = Direction.DOWN;
 					((BlockLiquid)world.getBlock(((int)(this.x/16)), ((int)((this.y/16)-1)))).state --;
 					this.state ++;
-					System.out.println("Son état est : " + ((BlockLiquid)world.getBlock(((int)(this.x/16)), ((int)((this.y/16)-1)))).state);
+					//System.out.println("Son état est : " + ((BlockLiquid)world.getBlock(((int)(this.x/16)), ((int)((this.y/16)-1)))).state);
 				}
 			}
 			
 			
 			
-			System.out.println("Mon état est : " + state);
+			//System.out.println("Mon état est : " + state);
 			iter = 0;
 		}
 		else
