b8880e277254da77d09d7594e53edfac8729503e
rodol <rodol@Rodolphe1997>
2018-01-04 00:09:09 +0100

    trying to make a fucking nut move

--- src/fr/utbm/entity/EntityAnimalBenenut.java
+++ src/fr/utbm/entity/EntityAnimalBenenut.java
@@ -2,6 +2,11 @@ package fr.utbm.entity;
 
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 import fr.utbm.block.BlockType;
 import fr.utbm.texture.TextureManager;
 import fr.utbm.world.World;
@@ -11,6 +16,7 @@ public class EntityAnimalBenenut extends EntityAnimal{
 	private int maturity;
 	private int growingTime;
 	private int readyToPlant;
+	private boolean hasJump = false;
 	
 	public EntityAnimalBenenut(float x, float y, World worldIn) {
 		
@@ -20,13 +26,23 @@ public class EntityAnimalBenenut extends EntityAnimal{
 		readyToPlant = r.nextInt(5000) + 5000;
 		maturity = 2;
 		text = TextureManager.getTexture(208 + maturity);
+		anim = new Animation[2];
+		anim[0] = TextureManager.getAnimation(16);
+		anim[1] = TextureManager.getAnimation(17);
+		maxHealth = 100;
+		health = 100;
+		directionX = 1;
+		activity = -1;
+		perform = false;
+		actionToPerform = 0;
+		directionToPerform = 1;
 	}
 	
 	public EntityAnimalBenenut(float x, float y, int m, World worldIn) {
 		
 		super(x, y, 15, 18, worldIn);
 		Random r = new Random();
-		growingTime = r.nextInt(10000) + 5000;
+		growingTime = r.nextInt(500) + 500;
 		readyToPlant = r.nextInt(5000) + 5000;
 		if(m > 2)
 		{
@@ -38,6 +54,16 @@ public class EntityAnimalBenenut extends EntityAnimal{
 		}
 		maturity = m;
 		text = TextureManager.getTexture(208 + maturity);
+		anim = new Animation[2];
+		anim[0] = TextureManager.getAnimation(16);
+		anim[1] = TextureManager.getAnimation(17);
+		maxHealth = 100;
+		health = 100;
+		directionX = 1;
+		activity = -1;
+		perform = false;
+		actionToPerform = 0;
+		directionToPerform = 1;
 	}
 	
 	public int getMaturity()
@@ -48,46 +74,62 @@ public class EntityAnimalBenenut extends EntityAnimal{
 	@Override
 	public void update()
 	{
-		//System.out.println("je suis dans le benenut : " + x/16 + " ; " + y/16 + "\nJe suis à la maturité : " + maturity + "\nMon nombre de ticks avant le lvl supérieur est : " + growingTime);
-		text = TextureManager.getTexture(208 + maturity);
-		
 		if(maturity < 2)
 		{
 			if(growingTime == 0)
 			{
 				maturity++;
 				Random r = new Random();
-				growingTime = r.nextInt(10000) + 5000;
+				growingTime = r.nextInt(500) + 500;
 			}
 			else
 			{
 				growingTime--;
 			}
+			text = TextureManager.getTexture(208 + maturity);
 		}
 		else
 		{
 			if(readyToPlant <= 0)
 			{
+				text = TextureManager.getTexture(208 + maturity);
 				readyToPlant = 0;
 				planting();
 			}
 			else
 			{
-				move();
+				//System.out.println("Dans le benenut en " + (int)(x/16) + " ; " + (int)(y/16) + " et le bloc de trou est ");
+				if(!perform){
+					if(directionToPerform == 1 && (world.getBlock((int)(((this.x+width)/16)), (int)(this.y/16)) == null || !world.getBlock((int)(((this.x+width)/16)), (int)(this.y/16)).isSolid()))
+					{
+						actionToPerform = 1;
+						directionToPerform = 1;
+						action(actionToPerform,directionToPerform);
+					}
+					else if(directionToPerform == 1 && world.getBlock((int)(((this.x+width)/16)), (int)(this.y/16)) != null && world.getBlock((int)(((this.x+width)/16)), (int)(this.y/16)).isSolid())
+					{
+						System.out.println("je vais essayer de sauter");
+						actionToPerform = 3;
+						directionToPerform = 1;
+						action(actionToPerform,directionToPerform);
+					}
+					else if ((world.getBlock((int)(((this.x)/16-1)), (int)(this.y/16)) == null || !world.getBlock((int)(((this.x)/16-1)), (int)(this.y/16)).isSolid())){
+						actionToPerform = 1;
+						directionToPerform = -1;
+						action(actionToPerform,directionToPerform);
+					}
+					else
+					{
+						directionToPerform = -directionToPerform;
+						activity = -1;
+					}
+				}
+				else
+				{
+		            action(actionToPerform, directionToPerform);
+		        }
 				readyToPlant--;
 			}
-		}
-	}
-	
-	public void move()
-	{
-		if(y%16 == 0 && (world.getBlock((int) x/16, (int) (y/16)-1)!=null) && (world.getBlock((int) x/16, (int) (y/16)-1).isSolid()))
-		{
-			
-		}
-		else
-		{
-			y-=2f;
 		}
 	}
 	
@@ -104,7 +146,55 @@ public class EntityAnimalBenenut extends EntityAnimal{
 		}
 		else
 		{
-			move();
+			//move();
+		}
+	}
+	
+	public void action(int actionID, int direction) {
+		switch (actionID) {
+		case 0 :	move(0,0,0);
+					break;
+					
+		case 1 :	if(isOnGround()) {
+						move(0.1f*direction, 0, 0);
+					} else {
+						move(0, 0, 0);
+					}
+					break;
+		
+		case 2: 	if (isOnGround()) {
+						move(0, 0, 1);
+					}
+					break;
+					
+		case 3:		if (isOnGround() && !hasJump) {
+						move(0.1f, 10f, 3);
+						hasJump = true;
+					} else {
+						move(0.3f * direction, 0, activity);
+					}
+					break;
+		}
+	}
+	
+	@Override
+	public void render(SpriteBatch sp) {
+		if (activity > -1) {
+			stateTime += Gdx.graphics.getDeltaTime();
+			TextureRegion currentFrame = anim[activity].getKeyFrame(stateTime, true);
+			if (directionX == -1) {
+				sp.draw(currentFrame, this.x, this.y);
+			} else if (directionX == 1) {
+				sp.draw(currentFrame, this.x + currentFrame.getRegionWidth(), this.y, -currentFrame.getRegionWidth(),
+						currentFrame.getRegionHeight());
+			}
+			perform = !anim[activity].isAnimationFinished(stateTime);
+		} else {
+			if (directionX == -1) {
+				sp.draw(this.text, this.x, this.y);
+			} else if (directionX == 1) {
+				sp.draw(this.text, this.x + this.text.getWidth(), this.y, -this.text.getWidth(), this.text.getHeight());
+			}
 		}
 	}
 }
