8ceec8569773e18caabb96970ca5b1223f4ef285
rodol <rodol@Rodolphe1997>
2018-01-04 16:02:59 +0100

    More life for stone + lava texture updated

--- src/fr/utbm/block/BlockStone.java
+++ src/fr/utbm/block/BlockStone.java
@@ -9,8 +9,8 @@ public class BlockStone extends Block{
 	{
 		super(x, y, TextureManager.getTexture(7), w);
 		this.blockId = 7;
-		this.blockHealth = 100;
-		this.maxHealth = 100;
+		this.blockHealth = 300;
+		this.maxHealth = 300;
 		this.blockType = BlockType.DIRT;
 		this.isGravitySensitive = false;
 		isSolid = true;
@@ -19,7 +19,7 @@ public class BlockStone extends Block{
 	public BlockStone(float x, float y, int bH, World w) {
 		super(x, y, TextureManager.getTexture(7), w);
 		this.blockId = 7;
-		this.maxHealth = 100;
+		this.maxHealth = 300;
 		this.blockHealth = bH;
 		this.blockType = BlockType.DIRT;
 		this.isGravitySensitive = false;
